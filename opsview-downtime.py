d439c428fe2f9788971da0cde028f02b10c1b006
Alex Vranceanu <alex@ioi.ro>
2013-11-29 01:32:54 +0200

    Improvements

--- opsview-downtime.py
+++ opsview-downtime.py
@@ -1,6 +1,6 @@
 import sys
 import argparse, getpass
-import urllib, urllib2
+import urllib2
 import json
 
 #Define global verbose variable
@@ -41,6 +41,9 @@ def getLoginToken(s,u,p):
     opsview_response = opsview.read()
     response = eval(opsview_response)
     
+    if verbose:
+        print("Login response:\n%s\n%s" % (opsview_response,response))
+    
     if not response:
         print("Unexpected message from Opsview server: %s" % response_text)
         sys.exit()
@@ -52,7 +55,7 @@ def getLoginToken(s,u,p):
         print("Opsview authentication failed with: %s" % response_text)
         sys.exit()
 
-    return token
+    return opsview_token
 
 #Schedule downtime function
 def scheduleDowntime(s,u,token,h,starttime,endtime,c):
@@ -60,6 +63,7 @@ def scheduleDowntime(s,u,token,h,starttime,endtime,c):
     opsview_user=u
     opsview_token=token
     opsview_host=h
+    opsview_starttime=starttime
     opsview_endtime=endtime
     opsview_comment=c
     global verbose
@@ -90,10 +94,11 @@ def scheduleDowntime(s,u,token,h,starttime,endtime,c):
     try:
         opsview_cookies = urllib2.HTTPCookieProcessor()
         opsview_opener = urllib2.build_opener(opsview_cookies)
-        opsview = opsview_opener.open(opeview_request)
+        opsview = opsview_opener.open(opsview_request)
     #Fail if connection error
     except urllib2.URLError, e:
         print("Could not schedule downtime for %s: %s: %s" % (opsview_host,e.code,e.read()))
+        sys.exit()
     
     print "Downtime request result:"
     print opsview.read()
@@ -103,48 +108,71 @@ def main():
     parser = argparse.ArgumentParser(description='Opsview Downtime Tool')
 
     #Define standard options
-    parser.add_argument('-u', '--user', nargs=1, action='store', dest='user', help='Opsview username', required=True, metavar='OpsviewUser')
-    parser.add_argument('-p', '--password', nargs=1, action='store', dest='password', help='Opsview password', required=False, metavar='OpsviewPassword')
-    parser.add_argument('-s', '--server', nargs=1, action='store', dest='server', help='Opsview server', default='https://localhost', required=False, metavar='OpsviewServer')
-    parser.add_argument('-c', '--comment', nargs=1, action='store', dest='comment', help='Downtime comment', default='Scheduled downtime.', required=False, metavar='DowntimeComment')
-    parser.add_argument('-t', '--starttime', nargs=1, action='store', dest='starttime', help='When to start the downtime. Default: now', default='now', required=False, metavar='DowntimeStartTime')
-    parser.add_argument('-T', '--endtime', nargs=1, action='store', dest='endtime', help='How much downtime (eg. +30m, +1h). Default: +2h', default='+2h', required=False, metavar='DowntimeEndTime')
+    parser.add_argument('-u', '--user', nargs=1, action='store', dest='user', help='Opsview username. Will prompt if not provided.', default=None, required=False, metavar='OpsviewUser')
+    parser.add_argument('-p', '--password', nargs=1, action='store', dest='password', help='Opsview password. Will prompt if not provided.', default=None, required=False, metavar='OpsviewPassword')
+    parser.add_argument('-s', '--server', nargs=1, action='store', dest='server', help='Opsview server', default=["https://localhost"], required=False, metavar='OpsviewServer')
+    parser.add_argument('-m', '--comment', nargs=1, action='store', dest='comment', help='Downtime comment', default=["Scheduled downtime."], required=False, metavar='DowntimeComment')
+    parser.add_argument('-t', '--starttime', nargs=1, action='store', dest='starttime', help='When to start the downtime. Default: now', default=["now"], required=False, metavar='DowntimeStartTime')
+    parser.add_argument('-T', '--endtime', nargs=1, action='store', dest='endtime', help='How much downtime (eg. +30m, +1h). Default: +2h', default=["+2h"], required=False, metavar='DowntimeEndTime')
     parser.add_argument('-v', '--verbose', action='store_true', help="Print more messages", default=False, required=False)
     
     #Define exclusive options (either -o or -f, cannot be used together)
     group=parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-o', '--opsviewhost', nargs=1, action='store', dest='opsviewhost', help='Host to schedule downtime for (the name of the host in Opsview)', required=False, metavar='Host')
-    group.add_argument('-f', '--file', nargs=1, action='store', dest='file', help='File to read hosts from', required=False, metavar="InputFile")
+    group.add_argument('-f', '--file', nargs=1, action='store', dest='file', help='File to read hosts from (one host per line)', required=False, metavar="InputFile")
 
     #Parse arguments
     args = parser.parse_args()
-    
-    #Read user password if not specified in arguments
-    if not args.password:
-        args.password = getpass.getpass()
-        while not args.password:
-            args.password = getpass.getpass()
-    
-    #TODO
-    if args.file:
-        print "This functionality is not yet implemented."
-        return 0;
     
     #Print verbose
     if args.verbose:
         global verbose
         verbose=True
+
+    if verbose:
+        print args
+    
+    #Read hosts from file
+    if args.file:
+        try:
+            opsview_hosts = [line.strip() for line in open(args.file[0])]
+        except:
+            print "Error reading file."
+            sys.exit()
+        if not opsview_hosts:
+            print "No hosts read from file."
+            sys.exit()
+    else:
+        opsview_hosts = args.opsviewhost
     
     if verbose:
-        print("\nServer: %s\nUsername: %s\nHost: %s\nPassword: %s" % (args.server[0],args.user[0],args.opsviewhost[0],str(args.password)))
-    
+        print "Hosts:"
+        for host in opsview_hosts:
+            print(host)
+
+    #Read user password if not specified in arguments
+    if not args.user:
+        args.user = [raw_input("Username: ")]
+        while not args.user[0]:
+            args.user = [raw_input("Username: ")]
+
+    #Read user password if not specified in arguments
+    if not args.password:
+        args.password = [getpass.getpass()]
+        while not args.password[0]:
+            args.password = [getpass.getpass()]
+
+    if verbose:
+        print("\nServer: %s\nUsername: %s\nPassword: %s\n" % (args.server[0],args.user[0],args.password[0]))
+
     #Get login token from Opsview
-    token = getLoginToken(args.server[0],args.user[0],str(args.password))
+    token = getLoginToken(args.server[0],args.user[0],args.password[0])
     if verbose:
         print("Token: %s" % token)
         
-    #Schedule downtime for host
-    scheduleDowntime(args.server[0],args.user[0],token,args.opsviewhost[0],args.starttime[0],args.endtime[0],args.comment[0])
+    #Schedule downtime for every host specified
+    for host in opsview_hosts:
+        scheduleDowntime(args.server[0],args.user[0],token,host,args.starttime[0],args.endtime[0],args.comment[0])
 
 if __name__ == "__main__":
     main()
